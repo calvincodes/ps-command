be0542c75386b741ec3bc08dfb72b61f67ceb452
calvincodes <arpitjain1821@gmail.com>
2018-09-23 20:34:37 -0500

    Able to parse hyphen as well

--- parse_cmd_args.c
+++ parse_cmd_args.c
@@ -8,14 +8,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include "cmd_line_args.h"
 
 int main(int argc, char *argv[]) {
-    int flags, opt;
-    int nsecs, tfnd;
 
-    nsecs = 0;
-    tfnd = 0;
-    flags = 0;
+    int opt;
 
     bool processIdFlag = 0; // Flag for process id argument
     bool stateFlag = 0; // Default = 0. Flag for state argument
@@ -23,25 +20,29 @@ int main(int argc, char *argv[]) {
     bool systemTimeFlag = 1; // Default = 1. Flag for userTime argument
     long pid;
 
-    while ((opt = getopt(argc, argv, "p:sU::S::vc")) != -1) {
+    while ((opt = getopt(argc, argv, "p:s::U::S::vc")) != -1) {
 
         switch (opt) {
 
             case 'p':
                 processIdFlag = 1;
                 pid = atoi(optarg);
+                printf("Detected p with process id %lu \n", pid);
                 break;
 
             case 's':
                 stateFlag = 1;
+                printf("Detected s \n");
                 break;
 
             case 'U':
                 userTimeFlag = '0';
+                printf("Detected U \n");
                 break;
 
             case 'S':
                 systemTimeFlag = '0';
+                printf("Detected S \n");
                 break;
 
             default: /* '?' */
@@ -51,18 +52,13 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("flags=%d; tfnd=%d; nsecs=%d; optind=%d\n",
-           flags, tfnd, nsecs, optind);
+//    if (optind >= argc) {
+//        fprintf(stderr, "Expected argument after options\n");
+//        exit(EXIT_FAILURE);
+//    }
 
-    if (optind >= argc) {
-        fprintf(stderr, "Expected argument after options\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("name argument = %s\n", argv[optind]);
-    printf("t argument = %s\n", argv[optind]);
-
-    /* Other code omitted */
+//    printf("name argument = %s\n", argv[optind]);
+//    printf("t argument = %s\n", argv[optind]);
 
     exit(EXIT_SUCCESS);
 }

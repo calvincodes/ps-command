7303fbf41993307ed33047ca62904705ccc4e2c2
calvincodes <arpitjain1821@gmail.com>
2018-09-23 21:33:40 -0500

    Adding getDefaultArg function

--- cmd_line_args.h
+++ cmd_line_args.h
@@ -10,13 +10,15 @@
 typedef struct cmdLineArg {
 
     bool processIdFlag; // Flag for process id argument
+    long pid;
     bool stateFlag; // Default = 0. Flag for state argument
     bool userTimeFlag; // Default = 1. Flag for userTime argument
     bool systemTimeFlag; // Default = 1. Flag for userTime argument
     bool vMemFlag; // Default = 0. Flag for virtual memory
     bool cmdLineFlag; // Default = 0. Flag for virtual memory
 
-    long pid;
 } cmdLineArg;
+
+cmdLineArg getDefaultInputArg();
 
 void toString(cmdLineArg arg);

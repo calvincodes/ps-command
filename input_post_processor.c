b7087e9b766f7efe9869d45ec5e814b518f06e67
calvincodes <arpitjain1821@gmail.com>
2018-09-26 14:32:36 -0500

    Setting pid in post processor

--- input_post_processor.c
+++ input_post_processor.c
@@ -3,9 +3,15 @@
 //
 
 #include <stddef.h>
+#include <stdlib.h>
 #include "input_post_processor.h"
 
 cmdLineArg getPostProcessedInput(cmdLineArg unprocessedInput) {
+
+    if (unprocessedInput.processIdFlag == true) {
+        char *ptr;
+        unprocessedInput.pid = strtol(unprocessedInput.processIdArg, &ptr, 10);
+    }
 
     if (unprocessedInput.userTimeOptArg != NULL && *unprocessedInput.userTimeOptArg == '-') {
         unprocessedInput.userTimeFlag = false;

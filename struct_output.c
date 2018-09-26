ad333769cfe62e568340c7c59f1d289f551a71fa
Anshu <anshu@MacBook-Pro.local>
2018-09-26 15:35:49 -0500

    Removing check from printouput

--- struct_output.c
+++ struct_output.c
@@ -7,30 +7,30 @@
 
 void print(struct struct_output output, cmdLineArg cmd_args) {
 
-    if(cmd_args.pid > -1) printf("%s", output.pid);
-    if(cmd_args.stateFlag) printf("\t%s", output.status);
-    if(cmd_args.userTimeFlag) printf("\t%s",output.utime);
-    if(cmd_args.systemTimeFlag) printf("\t%s", output.stime);
-    if(cmd_args.vMemFlag) printf("\t%s", output.virtual_memory);
-    if(cmd_args.cmdLineFlag) printf("\t%s", output.command_line);
+    printf("%s", output.pid);
+    if (cmd_args.stateFlag) printf("\t%s", output.status);
+    if (cmd_args.userTimeFlag) printf("\t%s", output.utime);
+    if (cmd_args.systemTimeFlag) printf("\t%s", output.stime);
+    if (cmd_args.vMemFlag) printf("\t%s", output.virtual_memory);
+    if (cmd_args.cmdLineFlag) printf("\t%s", output.command_line);
 }
 
 void print_test(struct struct_output output) {
 
     printf("\n");
-     printf("%s", output.pid);
+    printf("%s", output.pid);
     printf("\t%s", output.status);
-     printf("\t%s",output.utime);
+    printf("\t%s", output.utime);
     printf("\t%s", output.stime);
     printf("\t%s", output.virtual_memory);
-     printf("\t%s", output.command_line);
+    printf("\t%s", output.command_line);
 }
 
-void print_header(cmdLineArg cmd_args){
-    if(cmd_args.pid > -1) printf("%s", "PID");
-    if(cmd_args.stateFlag) printf("\t%s", "STATUS");
-    if(cmd_args.userTimeFlag) printf("\t%s", "UTIME");
-    if(cmd_args.systemTimeFlag) printf("\t%s", "STIME");
-    if(cmd_args.vMemFlag) printf("\t%s", "VMEM");
-    if(cmd_args.cmdLineFlag) printf("\t%s\n", "CMD");
+void print_header(cmdLineArg cmd_args) {
+    printf("%s", "PID");
+    if (cmd_args.stateFlag) printf("\t%s", "STATUS");
+    if (cmd_args.userTimeFlag) printf("\t%s", "UTIME");
+    if (cmd_args.systemTimeFlag) printf("\t%s", "STIME");
+    if (cmd_args.vMemFlag) printf("\t%s", "VMEM");
+    if (cmd_args.cmdLineFlag) printf("\t%s\n", "CMD");
 }
